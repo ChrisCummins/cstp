e91d8c6ba279fcf97fa6bfbfccfcc63d20c2bfd2
Chris Cummins <chrisc.101@gmail.com>
2014-01-05 17:05:49 +0000

    lib: Add a linked list testing routine
    This performs a simple stress test of a linked list, Testing all of the
    primary functions. Example output:
    
        0.000466 s      0.000002 s/entry        INSERT()        256 entries
        0.021367 s      0.000083 s/entry        SEARCH()        256 entries
        0.028131 s      0.000110 s/entry        GET()           256 entries
        0.226019 s      0.000226 s/entry        MINIMUM()       1000 times
        0.226727 s      0.000227 s/entry        MAXIMUM()       1000 times
        0.027246 s      0.000107 s/entry        SUCCESSOR()     254 entries
        0.027592 s      0.000109 s/entry        PREDECESSOR()   254 entries
        0.020856 s      0.000163 s/entry        DELETE()        128 entries
    
        0.000981 s      0.000002 s/entry        INSERT()        512 entries
        0.083565 s      0.000163 s/entry        SEARCH()        512 entries
        0.112170 s      0.000219 s/entry        GET()           512 entries
        0.452348 s      0.000452 s/entry        MINIMUM()       1000 times
        0.452331 s      0.000452 s/entry        MAXIMUM()       1000 times
        0.113076 s      0.000222 s/entry        SUCCESSOR()     510 entries
        0.111618 s      0.000219 s/entry        PREDECESSOR()   510 entries
        0.082561 s      0.000323 s/entry        DELETE()        256 entries
    
        0.002794 s      0.000003 s/entry        INSERT()        1024 entries
        0.332637 s      0.000325 s/entry        SEARCH()        1024 entries
        0.457697 s      0.000447 s/entry        GET()           1024 entries
        0.912682 s      0.000913 s/entry        MINIMUM()       1000 times
        0.907491 s      0.000907 s/entry        MAXIMUM()       1000 times
        0.458999 s      0.000449 s/entry        SUCCESSOR()     1022 entries
        0.460177 s      0.000450 s/entry        PREDECESSOR()   1022 entries
        0.332033 s      0.000649 s/entry        DELETE()        512 entries
    
        0.004073 s      0.000002 s/entry        INSERT()        2048 entries
        1.335263 s      0.000652 s/entry        SEARCH()        2048 entries
        1.882489 s      0.000919 s/entry        GET()           2048 entries
        1.846409 s      0.001846 s/entry        MINIMUM()       1000 times
        1.816114 s      0.001816 s/entry        MAXIMUM()       1000 times
        1.861576 s      0.000910 s/entry        SUCCESSOR()     2046 entries
        1.860991 s      0.000910 s/entry        PREDECESSOR()   2046 entries
        1.290618 s      0.001260 s/entry        DELETE()        1024 entries

--- lib.py
+++ lib.py
@@ -70,3 +70,102 @@ def test_hash_table(hash_table_class):
 
         # Check that looking up an item which doesn't exist returns nothing
         assert h.get(-1) == None
+
+# Accepts a linked list and element element class, and tests the
+# following linked list procedures:
+#
+#   list.insert(element)
+#   list.get(index)
+#   list.search(key)
+#   list.minimum()
+#   list.maximum()
+#   list.successor(element)
+#   list.predecessor(element)
+#   list.delete(element)
+#
+def test_linked_list(list, element_class):
+
+    for i in range(4):
+        test_size = 2 ** (i + 8)
+        data = get_random_int_array(test_size)
+        data_sorted = sorted(data)
+        data_min = min(data_sorted)
+        data_max = max(data_sorted)
+
+        # Test INSERT() routine:
+        t0 = time()
+        for i in data:
+            list.insert(element_class(i))
+        t1 = time()
+        elapsed_time = t1 - t0
+        print ("%f s\t%f s/entry\tINSERT()\t%d entries"
+               % (elapsed_time, elapsed_time / test_size, test_size))
+
+        # Test SEARCH routine:
+        t0 = time()
+        for i in data[:test_size / 2]:
+            assert list.search(i).key == i
+        t1 = time()
+        elapsed_time = t1 - t0
+        print ("%f s\t%f s/entry\tSEARCH()\t%d entries"
+               % (elapsed_time, elapsed_time / test_size, test_size))
+
+        # Check that get operation works:
+        t0 = time()
+        for i in range(test_size - 1):
+            assert list.get(i).key == data[test_size - 1 - i]
+        t1 = time()
+        elapsed_time = t1 - t0
+        print ("%f s\t%f s/entry\tGET()    \t%d entries"
+               % (elapsed_time, elapsed_time / test_size, test_size))
+
+        # Check that MINIMUM() works:
+        t0 = time()
+        for i in range(1000):
+            assert list.minimum().key == data_min
+        t1 = time()
+        elapsed_time = t1 - t0
+        print ("%f s\t%f s/entry\tMINIMUM()\t%d times"
+               % (elapsed_time, elapsed_time / 1000, 1000))
+
+        # Check that MAXIMUM() operation works:
+        t0 = time()
+        for i in range(1000):
+            assert list.maximum().key == data_max
+        t1 = time()
+        elapsed_time = t1 - t0
+        print ("%f s\t%f s/entry\tMAXIMUM()\t%d times"
+               % (elapsed_time, elapsed_time / 1000, 1000))
+
+        # Check that SUCCESSOR() operation works:
+        t0 = time()
+        for i in range(1, test_size - 2):
+            assert list.successor(list.get(i)).key == data[test_size - 2 - i]
+        t1 = time()
+        elapsed_time = t1 - t0
+        print ("%f s\t%f s/entry\tSUCCESSOR()\t%d entries"
+               % (elapsed_time, elapsed_time / (test_size - 2), test_size - 2))
+
+        # Check that PREDECESSOR() operation works:
+        t0 = time()
+        for i in range(1, test_size - 2):
+            assert list.predecessor(list.get(i)).key == data[test_size - i]
+        t1 = time()
+        elapsed_time = t1 - t0
+        print ("%f s\t%f s/entry\tPREDECESSOR()\t%d entries"
+               % (elapsed_time, elapsed_time / (test_size - 2), test_size - 2))
+
+        # Check that DELETE() operation works:
+        t0 = time()
+        for i in data[:test_size / 2]:
+            list.delete(list.search(i))
+        t1 = time()
+        elapsed_time = t1 - t0
+        print ("%f s\t%f s/entry\tDELETE()\t%d entries"
+               % (elapsed_time, elapsed_time / (test_size / 2), test_size / 2))
+
+        # Check that the remaining nodes still exist:
+        for i in data[test_size / 2:]:
+            assert list.search(i).key == i
+
+        print
