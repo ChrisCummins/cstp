8cb3667c5aa9a46c9e1673edb31f11f5ca9e8a7a
Chris Cummins <chrisc.101@gmail.com>
2014-01-07 01:15:50 +0000

    Import Python template file

---
+++ template.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#
+## TODO: Title:
+#
+# TODO: Description
+#
+## Performance:
+#
+# Worst case performance:       TODO:
+# Best case performance:        TODO:
+# Average case performance:     TODO:
+# Memory usage:                 TODO:
+#
+## Advantages:
+#
+# TODO: bullet point list of advantages
+#
+## Disadvantages:
+#
+# TODO: bullet point list of disadvantages
+#
+## References:
+#
+# TODO: List of references
+#
+## Code:
+
+
+if __name__ == "__main__":
+    # TODO: Simple test
